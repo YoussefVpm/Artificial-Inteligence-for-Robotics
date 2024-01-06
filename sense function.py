5a46dbf7957cac75715c9e637fe6af6ae09453c7
Youssef VPM <89054376+YoussefVpm@users.noreply.github.com>
2024-01-06 19:31:03 +0600

    Create sense function.py

---
+++ sense function.py
@@ -0,0 +1,29 @@
+#The function sense, takes p and Z as inputs, 
+#and outputs the NON-normalized 
+#probability distribution, q, after multiplying the entries 
+#in p by pHit or pMiss according to the color in the 
+#corresponding cell in world.
+
+#Probability of cells
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+
+World color definition
+world=['green', 'red', 'red', 'green', 'green']
+
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+q=[0]*len(p)
+
+#sense function
+def sense(p, Z):
+    #
+    for i in range(len(p)):
+        if world[i] == Z:
+            q[i] = p[i]*pHit
+        else:
+            q[i] = p[i]*pMiss
+    #
+    return q
+
+print sense(p,Z)
